bb84590557235ba06f715595f4da112754ea72be
aadzhanow <aadzhanow@gmail.com>
2026-06-08 23:22:44 +0200

    Initial Commit

---
+++ take-home-app/take_home_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  take_home_appApp.swift
+//  take-home-app
+//
+//  Created by Alisher on 08.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct take_home_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
