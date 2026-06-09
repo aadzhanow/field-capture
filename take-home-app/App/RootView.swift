d9117e6bb366a414a0f1c05033a665d3265db864
aadzhanow <aadzhanow@gmail.com>
2026-06-09 13:04:44 +0200

    Print app container paths on launch in debug builds

--- take-home-app/App/RootView.swift
+++ take-home-app/App/RootView.swift
@@ -10,7 +10,28 @@ struct RootView: View {
     var body: some View {
         GalleryView(diContainer: diContainer)
             .task {
+                #if DEBUG
+                printContainerPaths()
+                #endif
                 await diContainer.recoveryService.recover()
             }
     }
+
+    #if DEBUG
+    private func printContainerPaths() {
+        let appSupport = try? FileManager.default.url(
+            for: .applicationSupportDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        )
+        print("📦 App container")
+        print("   Home:        \(NSHomeDirectory())")
+        if let appSupport {
+            print("   App Support: \(appSupport.path)")
+            print("   Database:    \(appSupport.appendingPathComponent(Constants.databaseFileName).path)")
+            print("   Images:      \(appSupport.appendingPathComponent(Constants.imagesDirectoryName).path)")
+        }
+    }
+    #endif
 }
